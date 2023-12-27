decd63f2f8cf91867268331d3730ceba5fd2465d
Aman Verma <128216071+Aman5Techie@users.noreply.github.com>
2023-12-27 20:42:36 +0530

    Time: 9 ms (96.13%) | Memory: 47.5 MB (5.82%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int allowed_weight(int[] arr,int givensum){
+        int currentday= 0;
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(sum + arr[i] > givensum){
+                currentday++;
+                sum = arr[i];
+            }else{
+                sum += arr[i];
+
+            }
+        //     if(givensum<arr[i]){
+        //         return Integer.MAX_VALUE;
+        //     }
+        //     sum += arr[i];
+        //    if(sum==givensum){
+        //        currentday++;
+        //        sum = 0;
+        //    }else if(sum>givensum){
+        //        currentday++;
+        //        sum = 0;
+        //        sum += arr[i];
+        //    }
+         
+        }
+        if(sum != 0){
+            currentday++;
+        }
+        return currentday;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        if(weights.length == days){
+            int max = 0;
+           for(int i : weights){
+           max = Math.max(i,max);
+            }
+            return max;
+        }
+         int low = Integer.MIN_VALUE;
+        int high = 0;
+        for(int i : weights){
+            high += i;
+            low = Math.max(low,i);
+        }
+
+       
+        int answer = Integer.MAX_VALUE;
+
+        while(low<=high){
+            int mid = (low) + (high-low)/2;
+            int value = allowed_weight(weights,mid);
+            if(value<=days){
+                answer = Math.min(answer,mid);
+                high  = mid -1;
+            }else{
+                low = mid+1;
+            }
+        }
+
+        return  answer;
+        
+    }
+}
