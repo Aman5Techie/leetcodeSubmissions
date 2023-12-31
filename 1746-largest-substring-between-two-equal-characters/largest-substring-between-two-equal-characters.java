57fee628c171c507f54df241d48c2081620353e8
Aman Verma <128216071+Aman5Techie@users.noreply.github.com>
2024-01-01 01:43:24 +0530

    Time: 7 ms (14.21%) | Memory: 41.4 MB (11.72%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -1,32 +1,40 @@
 class Solution {
     public int maxLengthBetweenEqualCharacters(String s) {
-        HashMap<Character,Integer> check = new HashMap<Character,Integer>();
-        HashMap<Character,List<Integer>> double_map = new HashMap<Character,List<Integer>>();
-        for(int i = 0;i<s.length();i++){
-            if(check.containsKey(s.charAt(i))){
-                List<Integer> x = new ArrayList<Integer>();
-                if(double_map.containsKey(s.charAt(i))){
-                     x.add(check.get(s.charAt(i)));
-                     x.add(i);
-                     double_map.replace(s.charAt(i) ,x );
-                }else{
-                    x.add(check.get(s.charAt(i)));
-                    x.add(i);
-                    double_map.put(s.charAt(i) ,x);
-                }
+        // HashMap<Character,Integer> check = new HashMap<Character,Integer>();
+        // HashMap<Character,List<Integer>> double_map = new HashMap<Character,List<Integer>>();
+        // for(int i = 0;i<s.length();i++){
+        //     if(check.containsKey(s.charAt(i))){
+        //         List<Integer> x = new ArrayList<Integer>();
+        //         if(double_map.containsKey(s.charAt(i))){
+        //              x.add(check.get(s.charAt(i)));
+        //              x.add(i);
+        //              double_map.replace(s.charAt(i) ,x );
+        //         }else{
+        //             x.add(check.get(s.charAt(i)));
+        //             x.add(i);
+        //             double_map.put(s.charAt(i) ,x);
+        //         }
 
-            }else{
-                check.put(s.charAt(i),i);
+        //     }else{
+        //         check.put(s.charAt(i),i);
+        //     }
+        // }
+        // if(double_map.isEmpty()){
+        //     return -1;
+        // }
+        // int max_diff = 0;
+        // for(List<Integer> x : double_map.values()){
+        //     max_diff = Math.max(max_diff, (x.get(1) - x.get(0)) -1);
+        // }
+        int max = -1;
+        for(int i = 0;i<s.length();i++){
+            for(int j = s.length()-1;j>i;j--){
+                if(s.charAt(i) == s.charAt(j)){
+                    max = Math.max(max, j-i-1);
+                }
             }
         }
-        if(double_map.isEmpty()){
-            return -1;
-        }
-        int max_diff = 0;
-        for(List<Integer> x : double_map.values()){
-            max_diff = Math.max(max_diff, (x.get(1) - x.get(0)) -1);
-        }
-        
-        return max_diff;
+        return max;
+        // return max_diff;
     }
 }
