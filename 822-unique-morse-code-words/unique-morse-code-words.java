7702eea2b14078df65e736912d485dc13a5292ea
Aman Verma <128216071+Aman5Techie@users.noreply.github.com>
2024-01-06 10:47:05 +0530

    Time: 3 ms (41.35%) | Memory: 41.3 MB (30.79%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.java
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -1,13 +1,17 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {//97
         String[]  codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        Set<String> set = new HashSet<String>();
+        // Set<String> set = new HashSet<String>();
+        HashMap<String,Boolean> set = new   HashMap<String,Boolean>();
         for(String word : words){
             String code = "";
             for(int i = 0 ;i<word.length();i++){
                 code += codes[(word.charAt(i) - 0)-97];
             }
-            set.add(code);
+            if(!set.containsKey(code)){
+
+            set.put(code,true);
+            }
         }
      
         return set.size();
