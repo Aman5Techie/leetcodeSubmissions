8e905affb1fc768437f1e6e3e783f2eef94b79f1
Aman Verma <128216071+Aman5Techie@users.noreply.github.com>
2024-01-09 14:53:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (32.25%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -12,25 +12,26 @@ class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         ListNode ptr = head;
         ListNode last= null;
-        ListNode copy_of_head = new ListNode(5);
-        copy_of_head.next = head;
         int length = 0;
+
         while(ptr != null){
             length++;
             last = ptr;
             ptr = ptr.next;
         }
-        if(length==0 ){
+        if(length==0){
             return head;
         }
-   
-        if(k > length){
+         if(k > length){
             k =  k%length ;
         }
+   
         if(length==1 || k==0  || k == length){
             return head;
         }
+
         ptr = head;
+        
         ListNode ptr2 = head;
      
         for(int i = 0;i<length-k;i++){
@@ -41,9 +42,7 @@ class Solution {
 
         ptr2.next = null;
         last.next = head;
-        if(ptr==null){
-            return copy_of_head;
-        }
+
         return ptr;
     }
 }
